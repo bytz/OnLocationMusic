97afd0497546fb42877e45a7a199e822f9b94f34
Kenshin <himura.kenshin.aj@gmail.com>
2013-05-20 00:14:10 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,31 +1,6 @@
   </div>
 </div>
 <div id="footer">
-	<div class="container">
-	    <div class="span-1">
-			<h3>Developers</h3>
-			<ul>
-				<li><a href="http://alexandru.jurubita.ro">Alexandru Jurubita</a></li>
-				<li><a href="http://andrei.jurubita.ro">Andrei Jurubita</a></li>
-				<li><a href="#">Vlad Teodorescu</a></li>
-			</ul>
-		</div>
-	    <div class="span-1">
-			<h3>Powered by</h3>
-			<ul>
-				<li><a href="http://developer.yahoo.com/yql/">Yahoo! Query Language</a></li>
-				<li><a href="http://developer.yahoo.com/mediaplayer/">Yahoo! Media Player</a></li>
-				<li><a href="http://www.last.fm/">last.fm</a></li>
-			</ul>
-		</div>
-	    <div class="span-3 last">
-			<h3>Source Code</h3>
-			<ul>
-				<li><a href="www.github.com">github.com</a></li>
-				<li><a href="#"></a></li>
-			</ul>
-		</div>
-	</div>
 </div>
 </body>
 </html>
